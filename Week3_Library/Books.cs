27eb595a170b30fed84536d4b3169dd27d66c1f0
csf23moj <csf23moj@MF124PC17>
2024-10-08 11:50:05 +0100

    Properties and encapsulation

--- Week3_Library/Books.cs
+++ Week3_Library/Books.cs
@@ -2,17 +2,34 @@
 {
     class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title; // variable
+        private string author; // variable
+        private string isbn; // variable
 
-        // Example of a constructor that allows us to 'construct' a new
-        // Book object
+        // Title property to allow access
+        // to the title private variable
+        public string Title
+        {
+            get { return Title; } // get method
+            set { Title = value; } // set method
+        }
+        public string Author
+        {
+            get { return Author; } // get method
+            set { Author = value; } // set method
+        }
+        public string ISBN
+        {
+            get { return ISBN; } // get method
+            set { ISBN = value; } // set method
+        }
+
+        // Constructor to add a new book
         public Book(string bookTitle, string bookAuthor, string bookISBN)
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            this.title = bookTitle;
+            this.author = bookAuthor;
+            this.isbn = bookISBN;
         }
 
         // Example of a method within the Book class
