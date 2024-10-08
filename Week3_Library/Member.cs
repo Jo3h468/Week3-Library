7a2199006a5e4349d9fd0700a27dcc8d721e279f
csf23moj <csf23moj@MF124PC17>
2024-10-08 12:02:17 +0100

    Adding member class

---
+++ Week3_Library/Member.cs
@@ -0,0 +1,51 @@
+﻿namespace Week3_Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        //Public properties
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Pivate makes it read only
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+        // Constructor for new member
+        public Member(int memberId, string name, string address, int phone, int memberId, string name, string address, int phone)
+        {
+            this.memberId = memberId;
+            this.name = name;
+            this.address = address;
+            this.phone = phone;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book author: {Author}");
+            Console.WriteLine($"Book ISBN: {ISBN}");
+            Console.WriteLine();
+        }
+}
